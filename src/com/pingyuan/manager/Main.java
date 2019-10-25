c7bd3768091c8637a78390fa78f6bc443b564ccd
sflyhuan <sflyhuan@163.com>
2019-10-25 17:47:07 +0800

    first

---
+++ src/com/pingyuan/manager/Main.java
@@ -0,0 +1,25 @@
+package com.pingyuan.manager;
+
+import com.pingyuan.manager.main.MainJFrame;
+import org.jb2011.lnf.beautyeye.BeautyEyeLNFHelper;
+
+import javax.swing.*;
+import javax.swing.plaf.InsetsUIResource;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try {
+
+            BeautyEyeLNFHelper.frameBorderStyle = BeautyEyeLNFHelper.FrameBorderStyle.generalNoTranslucencyShadow;
+            BeautyEyeLNFHelper.launchBeautyEyeLNF();
+            UIManager.put("RootPane.setupButtonVisible",false);
+            UIManager.put("TabbedPane.tabAreaInsets",new InsetsUIResource(3, 0, 2, 20));
+        } catch (Exception e) {
+            //TODO exception
+        }
+
+        new MainJFrame();
+    }
+
+}
